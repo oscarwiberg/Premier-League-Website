2f15f20148a85333766bf82a5c6e812c9fda37c0
oscarwiberg <oscar.wiberg@live.se>
2020-07-21 18:52:25 +0200

    recieving photo data

--- premierleaguewebsite/src/components/home/NewsItem.jsx
+++ premierleaguewebsite/src/components/home/NewsItem.jsx
@@ -5,17 +5,29 @@ import './NewsItem.css';
 
 const NewsItem = () => {
   const [posts, setPosts] = useState([]);
-  const [photo, setPhoto] = useState([]);
+  const [photos, setPhoto] = useState([]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10')
       .then((response) => response.json())
       .then((json) => setPosts(json));
   }, []);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=10')
+      .then((response) => response.json())
+      .then((json) => setPhoto(json));
+  }, []);
+
+  const photoItems = photos.map((photo) => (
+    <div key={photo.id} className="Photo">
+      <img src={photo.url} alt="Photo" />
+    </div>
+  ));
+
   const postItems = posts.map((post) => (
     <div key={post.id} className="Card">
-      <h3>{post.title}</h3>
+      <h4>{post.title}</h4>
       <p>{post.body}</p>
     </div>
   ));
