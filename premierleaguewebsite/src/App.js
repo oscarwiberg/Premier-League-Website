7a6e148b99f3b0933cd05cb8797702cc8e1a148f
oscarwiberg <oscar.wiberg@live.se>
2020-08-05 18:13:11 +0200

    starting to implement redux

--- premierleaguewebsite/src/App.js
+++ premierleaguewebsite/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './App.css';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Menu from './components/Menu';
@@ -9,6 +12,7 @@ import Teams from './components/teams/Teams';
 import TopScorers from './components/topScorers/TopScorers';
 import Games from './components/games/Games';
 import Forum from './components/forum/Forum';
+import store from './store';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -24,20 +28,22 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Router>
-        <Header toggleMenuHandler={toggleMenuHandler} menuOpen={menuOpen} />
-        {menu}
-        <div className="Container">
-          <Route path="/" exact component={Home} />
-          <Route path="/teams" component={Teams} />
-          <Route path="/topscorers" component={TopScorers} />
-          <Route path="/games" component={Games} />
-          <Route path="/forum" component={Forum} />
-        </div>
-        <Footer />
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Header toggleMenuHandler={toggleMenuHandler} menuOpen={menuOpen} />
+          {menu}
+          <div className="Container">
+            <Route path="/" exact component={Home} />
+            <Route path="/teams" component={Teams} />
+            <Route path="/topscorers" component={TopScorers} />
+            <Route path="/games" component={Games} />
+            <Route path="/forum" component={Forum} />
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
