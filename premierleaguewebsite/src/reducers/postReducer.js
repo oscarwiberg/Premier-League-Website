3c06fb279d73d07139bda7cb6c508a43983cfaa4
oscarwiberg <oscar.wiberg@live.se>
2020-08-06 12:27:31 +0200

    working on postReducer

--- premierleaguewebsite/src/reducers/postReducer.js
+++ premierleaguewebsite/src/reducers/postReducer.js
@@ -1,1 +1,19 @@
 import { FETCH_POSTS, NEW_POST } from '../actions/types';
+
+const initialState = {
+  items: [],
+  item: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      break;
+
+    case NEW_POST:
+      break;
+
+    default:
+      return state;
+  }
+}
